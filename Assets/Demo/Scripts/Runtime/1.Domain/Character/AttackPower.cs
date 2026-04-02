82b07bebfaaffe7baf1d1b85aa6bb036109d1c8e
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-02 11:03:00 +0900

    [add]攻撃力のVO追加

---
+++ Assets/Demo/Scripts/Runtime/1.Domain/Character/AttackPower.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Demo.Domain
+{
+    public readonly struct AttackPower
+    {
+        public AttackPower(float value)
+        {
+            _value = value;
+        }
+
+        public float Value => _value;
+
+        public static float operator *(AttackPower power, float multiplier)
+        {
+            return power._value * multiplier;
+        }
+
+        private readonly float _value;
+    }
+}
