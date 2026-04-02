38e544b3f5c6378da40ca93ab03bde03b4f9a822
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-03 04:51:56 +0900

    [update]DTOを使用するように

--- Assets/Demo/Scripts/Runtime/3.Adaptor/IUnitSpawnSignal.cs
+++ Assets/Demo/Scripts/Runtime/3.Adaptor/IUnitSpawnSignal.cs
@@ -1,7 +1,14 @@
 namespace Demo.Adaptor
 {
+    /// <summary>
+    /// ユニット生成を通知するSignalインターフェース。
+    /// </summary>
     public interface IUnitSpawnSignal
     {
-        public void InvokeSpawnUnit(string[] characterIDs);
+        /// <summary>
+        /// ユニット生成を呼び出す。
+        /// </summary>
+        /// <param name="dto">生成情報のDTO</param>
+        public void InvokeSpawnUnit(in UnitSpawnDTO dto);
     }
 }
