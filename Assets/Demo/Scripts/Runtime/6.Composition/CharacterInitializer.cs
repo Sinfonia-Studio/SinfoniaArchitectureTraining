fe947c7140bd69fac476802900fac9ce4aa4e0e5
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-03 04:55:35 +0900

    [update]初期化メソッドを追加

--- Assets/Demo/Scripts/Runtime/6.Composition/CharacterInitializer.cs
+++ Assets/Demo/Scripts/Runtime/6.Composition/CharacterInitializer.cs
@@ -7,17 +7,29 @@ using CharacterController = Demo.Adaptor.CharacterController;
 
 namespace Demo.Composition
 {
+    /// <summary>
+    /// キャラクターの初期化を行うクラス。
+    /// </summary>
     public static class CharacterInitializer
     {
         /// <summary>
-        /// キャラクターの初期化と依存性注入を行う
+        /// キャラクターの初期化と依存性注入を行う。
         /// </summary>
         /// <param name="asset">キャラクターの基礎データ</param>
         /// <param name="view">キャラクターの表示側</param>
         public static void GenerateCharacter(CharacterAsset asset, CharacterView view)
         {
             CharacterEntity entity = asset.Get();
+            Initialize(entity, view);
+        }
 
+        /// <summary>
+        /// 既存のエンティティを使用してキャラクターを初期化する。
+        /// </summary>
+        /// <param name="entity">キャラクターエンティティ</param>
+        /// <param name="view">キャラクターの表示側</param>
+        public static void Initialize(CharacterEntity entity, CharacterView view)
+        {
             CharacterState state = new(entity);
 
             CharacterViewModel viewModel = new();
