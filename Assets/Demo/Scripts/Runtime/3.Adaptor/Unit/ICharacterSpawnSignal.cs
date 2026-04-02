bd634e14c6b80fb4857844f7d829570e3989dd45
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-03 06:43:19 +0900

    [add]シグナルを追加

---
+++ Assets/Demo/Scripts/Runtime/3.Adaptor/Unit/ICharacterSpawnSignal.cs
@@ -0,0 +1,15 @@
+namespace Demo.Adaptor
+{
+    /// <summary>
+    /// キャラクターの準備が整ったことをView層へ通知するSignalインターフェース。
+    /// </summary>
+    public interface ICharacterSpawnSignal
+    {
+        /// <summary>
+        /// キャラクターの準備完了を通知する。
+        /// </summary>
+        /// <param name="characterID">キャラクターID</param>
+        /// <param name="adaptor">生成されたAdaptor</param>
+        void OnCharacterReady(string characterID, CharacterAdaptor adaptor);
+    }
+}
