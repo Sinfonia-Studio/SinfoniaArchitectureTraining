5272499cd9644ec923dca07ff53a721445f385cf
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-03 04:55:09 +0900

    [update]DTOを使用して発火する

--- Assets/Demo/Scripts/Runtime/4.View/UnitSpawnSignal.cs
+++ Assets/Demo/Scripts/Runtime/4.View/UnitSpawnSignal.cs
@@ -3,13 +3,22 @@ using System;
 
 namespace Demo.View
 {
+    /// <summary>
+    /// ユニット生成を通知するSignal。
+    /// AdaptorからViewへイベントを仲介する。
+    /// </summary>
     public class UnitSpawnSignal : IUnitSpawnSignal
     {
-        public Action<string[]> OnSpawn;
+        /// <summary> ユニット生成時のイベント </summary>
+        public event Action<UnitSpawnDTO> OnSpawn;
 
-        public void InvokeSpawnUnit(string[] characterIDs)
+        /// <summary>
+        /// ユニット生成を呼び出す。
+        /// </summary>
+        /// <param name="dto">生成情報のDTO</param>
+        public void InvokeSpawnUnit(in UnitSpawnDTO dto)
         {
-            OnSpawn?.Invoke(characterIDs);
+            OnSpawn?.Invoke(dto);
         }
     }
 }
