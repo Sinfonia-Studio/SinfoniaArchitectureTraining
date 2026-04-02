896f2bdccb1d02a1196d925434415831858fd5e8
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-03 02:06:29 +0900

    [add]キャラクターのデータベースを追加

---
+++ Assets/Demo/Scripts/Runtime/4.View/Character/CharacterDataBase.cs
@@ -0,0 +1,34 @@
+using Demo.Utility;
+using System;
+using UnityEngine;
+
+namespace Demo.View
+{
+    [CreateAssetMenu(fileName = nameof(CharacterDataBase),
+        menuName = PathConst.CREATE_ASSET_MENU_PATH + nameof(CharacterDataBase),
+        order = 0)]
+    public class CharacterDataBase : ScriptableObject
+    {
+        public bool TryGetCharacterView(string id, out CharacterView view)
+        {
+            CharacterData data = Array.Find(_characterDataBase, data => data.ID == id);
+            view = data.View;
+            return view != null;
+        }
+
+        [SerializeField]
+        private CharacterData[] _characterDataBase;
+
+        [Serializable]
+        private struct CharacterData
+        {
+            public string ID => _id;
+            public CharacterView View => _view;
+
+            [SerializeField]
+            private string _id;
+            [SerializeField]
+            private CharacterView _view;
+        }
+    }
+}
