086f69ce771e86de0b4abbf712daa5fa73e669fc
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-02 11:40:16 +0900

    [update]クリ率とクリダメを追加

--- Assets/Demo/Scripts/Runtime/1.Domain/Character/IAttacker.cs
+++ Assets/Demo/Scripts/Runtime/1.Domain/Character/IAttacker.cs
@@ -3,5 +3,7 @@ namespace Demo.Domain
     public interface IAttacker
     {
         public AttackPower AttackPower { get; }
+        public CriticalChance CriticalChance { get; }
+        public CriticalDamage CriticalDamage { get; }
     }
 }
