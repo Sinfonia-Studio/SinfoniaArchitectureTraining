be9b7a38873eed9a5818ecc88f26a1bd38ca7ad2
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-04 22:12:28 +0900

    [update]名前を変更

--- Assets/Demo/Scripts/Runtime/3.Adaptor/Unit/ICharacterAdaptorDriver.cs
+++ Assets/Demo/Scripts/Runtime/3.Adaptor/Unit/ICharacterAdaptorDriver.cs
@@ -1,15 +1,17 @@
+using Demo.Domain;
+
 namespace Demo.Adaptor
 {
     /// <summary>
     /// キャラクターのAdaptorを生成するファクトリインターフェース。
     /// </summary>
-    public interface ICharacterAdaptorFactory
+    public interface ICharacterAdaptorDriver
     {
         /// <summary>
         /// エンティティからAdaptorを生成する。
         /// </summary>
         /// <param name="entity">エンティティ</param>
         /// <returns>Adaptorコンテナ</returns>
-        CharacterAdaptor Create(Domain.CharacterEntity entity);
+        public CharacterAdaptor Create(CharacterEntity entity);
     }
 }
