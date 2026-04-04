c725ff494aef955b039c2388865094709ad26def
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-04 22:51:28 +0900

    [update]イコールをオーバーライド

--- Assets/Demo/Scripts/Runtime/1.Domain/Character/CharacterID.cs
+++ Assets/Demo/Scripts/Runtime/1.Domain/Character/CharacterID.cs
@@ -8,6 +8,8 @@ namespace Demo.Domain
 
         public static bool operator ==(CharacterID left, CharacterID right) => left._value == right._value;
         public static bool operator !=(CharacterID left, CharacterID right) => !(left == right);
+        public override bool Equals(object obj) => obj is CharacterID other && this == other;
+        public override int GetHashCode() => _value.GetHashCode();
 
         private readonly string _value;
     }
