5821f888a0d36eaf095b63e76314e5dd5d9d975e
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-04 23:00:28 +0900

    [update]ドライバーを実装

--- Assets/Demo/Scripts/Runtime/6.Composition/CharacterAdaptorDriver.cs
+++ Assets/Demo/Scripts/Runtime/6.Composition/CharacterAdaptorDriver.cs
@@ -1,6 +1,9 @@
 using Demo.Adaptor;
+using Demo.Application;
 using Demo.Domain;
 using Demo.View;
+using UnityEngine;
+using CharacterController = Demo.Adaptor.CharacterController;
 
 namespace Demo.Composition
 {
@@ -8,22 +11,31 @@ namespace Demo.Composition
     /// キャラクターのAdaptorを生成する具体的なファクトリ。
     /// レイヤーを跨いで必要な各クラスをインスタンス化する。
     /// </summary>
-    public class CharacterAdaptorFactory : ICharacterAdaptorFactory
+    public class CharacterAdaptorDriver : ICharacterAdaptorDriver
     {
+        public CharacterAdaptorDriver(CharacterMovementService movementService, ICharacterRepository repository)
+        {
+            _movementService = movementService;
+            _repository = repository;
+        }
+
         /// <summary>
         /// エンティティから、各レイヤーのパーツ(State, VM, Presenter, Controller)を生成し、
         /// Adaptorコンテナにまとめて返す。
         /// </summary>
-        public CharacterAdaptor Create(CharacterEntity entity)
+        public CharacterAdaptor Create(CharacterEntity entity, Vector3 position)
         {
             // 1. 各レイヤーのパーツを生成
-            CharacterState state = new(entity);
+            CharacterState state = new(entity, position);
             CharacterViewModel viewModel = new(); // View層の実体
             CharacterPresenter presenter = new(state, viewModel);
-            CharacterController controller = new(state, presenter);
+            CharacterController controller = new(state, presenter, _movementService, _repository);
 
             // 2. Adaptorコンテナとして返す
             return new CharacterAdaptor(viewModel, controller);
         }
+
+        private readonly CharacterMovementService _movementService;
+        private readonly ICharacterRepository _repository;
     }
 }
